48a86995257a5fd5155145b953366c67a502d35b
Francis Tyers <ftyers@prompsit.com>
2019-04-09 20:26:50 +0100

    add initial sentences

---
+++ not-to-release/corpus-martinik.py
@@ -0,0 +1,38 @@
+import sys, re 
+
+sent_idx = 0
+
+def guess_tag(idx, s):
+	tag = '_'
+
+	if s in "!\"'()+,./:?«»–—‘“”•…":
+		return 'PUNCT'
+	elif idx != 1 and s[0].isupper():
+		return 'PROPN'
+	elif re.match('^[0-9]+$', s):
+		return 'NUM'
+
+	return tag
+
+for line in sys.stdin.readlines():
+# !"'()+,./:?«»–—‘“”•…  0123456789
+	orig_line = line.strip('\n')
+	for c in "!\"'()+,./:?«»–—‘“”•…":
+		line = line.replace(c, ' ' + c + ' ')
+		line = re.sub('  *', ' ', line)
+
+	line = line.strip()
+
+	print('# sent_id = %d' % sent_idx)
+	print('# text = %s' % orig_line)
+	idx = 0
+	for tok in line.split(' '):
+		idx += 1
+		tag = guess_tag(idx, tok)
+		lemma = tok
+		#       1    2    3    4    5   6     7    8    9    10
+		row = (idx, tok, lemma, tag, '_', '_', '_', '_', '_', '_')
+		print('%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % row)
+
+	print('')
+	sent_idx = sent_idx + 1
