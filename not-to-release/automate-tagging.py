6d31480c7c52bc18e4945b66722170c943b06417
Francis Tyers <ftyers@prompsit.com>
2019-04-23 20:30:03 +0100

    add automate tagging script

---
+++ not-to-release/automate-tagging.py
@@ -0,0 +1,44 @@
+import sys, re
+
+dico = {}
+freq = {}
+
+for line in open(sys.argv[1]).readlines():
+	if line.strip() == '':
+		continue
+	if line[0] == '#': 
+		continue
+	row = line.split('\t')
+	form = row[1]
+	tag = row[4]
+
+	if tag == '_':
+		continue
+	
+	if form not in dico:
+		dico[form] = {}
+	if tag not in dico[form]:
+		dico[form][tag] = 0	
+		freq[form] = (0, 'X')
+
+	dico[form][tag] += 1
+
+	if dico[form][tag] >= freq[form][0]:
+		freq[form] = (dico[form][tag], tag)
+
+for line in sys.stdin.readlines():
+	line = line.strip('\n')
+	if line == '':
+		print()
+		continue
+	if line[0] == '#':
+		print(line)
+		continue
+
+	row = line.split('\t')
+	form = row[1]
+	if form in freq and row[3] == '_':
+		row[9] = 'GuessedTag=' + freq[form][1] + ',' + str(freq[form][0])
+
+	print('\t'.join(row))
+
